99bd09046219d8ef08ac40402783b01754c1089f
tino <vovthuy@gmail.com>
2020-08-05 11:02:00 -0700

    handles bot prefix command better

--- src/main/java/com/discordchatbot/commands/ChatCommand.java
+++ src/main/java/com/discordchatbot/commands/ChatCommand.java
@@ -2,6 +2,7 @@ package com.discordchatbot.commands;
 
 import javax.annotation.Nonnull;
 
+import com.discordchatbot.DiscordChatBot.Config;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -15,15 +16,16 @@ public class ChatCommand extends ListenerAdapter{
 			return;
 		}
 		
+		if ( event.getMessage().getContentRaw().startsWith(Config.get("PREFIX")) )
+			return;
+		
 		if (ToggleChatCommand.chatEnable) {
-			//event.getChannel().sendMessage("hello this is a text test!").queue();
 			String message = event.getMessage().getContentRaw();
 			String response = ToggleChatCommand.chatSession.multisentenceRespond(message);
 			event.getChannel().sendMessage(response).queue();
 			
 			System.out.println("you: " + message);
 			System.out.println("bot: " + response);
-			
 		}
 	}
 	
--- src/main/java/com/discordchatbot/commands/ToggleChatCommand.java
+++ src/main/java/com/discordchatbot/commands/ToggleChatCommand.java
@@ -29,28 +29,23 @@ public class ToggleChatCommand extends Command{
 	protected void execute(CommandEvent event) {
 		String[] args = event.getMessage().getContentRaw().split(" ");
 		
-		
-		
 		if (args.length < 2) {
 			event.reply("```Please provide an argument:\n" + 
 					Config.get("PREFIX") + name + " " + getArguments() +"```");
 			return;
 		}
 			
-		
 		if(args[1].equalsIgnoreCase("off")) {
-			//event.reply("turning chat function off");
 			bot = null;
 			chatSession = null;
 			System.gc();
 			chatEnable = false;
-			System.out.println("chat off");
 			
+			System.out.println("chat off");
 			event.reply("```Chat function off```");
 		}
 		else if (args[1].equalsIgnoreCase("on")){
-			//event.reply("turning chat function on");
-			
+
 			resourcesPath = getResourcesPath();
 			bot = new Bot(botName, resourcesPath);
 			chatSession = new Chat(bot);
@@ -58,13 +53,12 @@ public class ToggleChatCommand extends Command{
 			MagicBooleans.trace_mode = TRACE_MODE;
 			chatEnable = true;
 			
+			event.reply("```Chat function on```");
 			System.out.println("chat on");
 		}
-		
 	}
 	
 	private static String getResourcesPath() {
-
 		// getting base directory of the project
 		File currDir = new File(".");
 		String path = currDir.getAbsolutePath();
