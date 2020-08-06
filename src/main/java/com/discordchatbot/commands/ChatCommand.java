d34484115089688c8ccc491b974287c485d3a110
tino <vovthuy@gmail.com>
2020-08-06 14:18:57 -0700

    handles multiple setnences response better

--- src/main/java/com/discordchatbot/commands/ChatCommand.java
+++ src/main/java/com/discordchatbot/commands/ChatCommand.java
@@ -1,5 +1,7 @@
 package com.discordchatbot.commands;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.annotation.Nonnull;
 
 import com.discordchatbot.DiscordChatBot.Config;
@@ -10,7 +12,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class ChatCommand extends ListenerAdapter{
 
 
-	public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
+	public void onMessageReceived(@Nonnull MessageReceivedEvent event) throws IllegalArgumentException{
 		
 		if(event.getAuthor().isBot()) {
 			return;
@@ -19,13 +21,29 @@ public class ChatCommand extends ListenerAdapter{
 		if ( event.getMessage().getContentRaw().startsWith(Config.get("PREFIX")) )
 			return;
 		
-		if (ToggleChatCommand.chatEnable) {
-			String message = event.getMessage().getContentRaw();
-			String response = ToggleChatCommand.chatSession.multisentenceRespond(message);
-			event.getChannel().sendMessage(response).queue();
-			
-			System.out.println("you: " + message);
-			System.out.println("bot: " + response);
+		try {
+			if (ToggleChatCommand.chatEnable) {
+				//splits sentences ending with . ? !
+				String[] msg = event.getMessage().getContentRaw().split("\\.|\\?|\\!");
+				String message;
+				String response;
+
+				for(int i = 0; i < msg.length; i++) {
+					message = msg[i];
+					response = ToggleChatCommand.chatSession.multisentenceRespond(message);
+					event.getChannel().sendMessage(response).queue();
+					
+					System.out.println("you: " + message);
+					System.out.println("bot: " + response);
+					
+					TimeUnit.SECONDS.sleep(1);
+				}
+			}
+		}
+		catch (IllegalArgumentException e) {
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 	
